f154fa79668dc324dec8be1342a1e4a24e6848c3
Maxim <ymaax@mail.ru>
2013-09-21 01:10:47 +0700

    dir traverse

--- httpserv/src/ReqProcessor.java
+++ httpserv/src/ReqProcessor.java
@@ -1,19 +1,34 @@
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 
 public class ReqProcessor {
 
-    public char[] getBytes(String path) {
+    public void getBytes(String path) {
         ArrayList<String[]> files = new ArrayList<String[]>();
         ArrayList<String> dirs = new ArrayList<String>();
+        if (new File(path).getParent() != null) {
+            dirs.add("..");
+        }
         for (File f: new File(path).listFiles()) {
             if (f.isDirectory()) {
                 dirs.add(f.getName());
             }
             else {
-                String[] finfo = {f.getName(), f.lastModified(), f.length()};
+                String[] finfo = {f.getName(), new SimpleDateFormat("HH:mm:ss dd.MM.yyyy").format(
+                        new Date(f.lastModified())).toString(), new Long(f.length()).toString()};
                 files.add(finfo);
             }
         }
+        Collections.sort(files, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return o1[0].compareTo(o2[0]);
+            }
+        });
+        Collections.sort(dirs);
     }
 }
