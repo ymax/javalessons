cdb0a87de738d262ac0e2d342d15023b0877ab0c
Maxim <ymaax@mail.ru>
2013-10-03 22:55:10 +0700

    jdbc rework

---
+++ jdbchost/src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Макс
+ * Date: 03.10.13
+ * Time: 22:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        GuestBookController gbc = new MyGuestBookController();
+
+    }
+}
